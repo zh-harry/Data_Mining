999d80307ec854cabb3d12d689c5404740fffb6c
zh-harry <kingkie729@gmail.com>
2026-06-14 16:40:43 +0800

    output final result

--- final/clustering/kmeans/main.cpp
+++ final/clustering/kmeans/main.cpp
@@ -2,6 +2,7 @@
 #include "datareader.h"
 #include "kmeans.h"
 #include <iostream>
+#include <unordered_map>
 
 int main(int argc, char* argv[]){
     dataset classify_results;
@@ -27,15 +28,72 @@ int main(int argc, char* argv[]){
     std::vector<std::size_t> labels = kmeans.fit(unknowns);
 
     // output result (labels)
+    dataset final_result = classify_results;
     for(std::size_t i = 0;i < labels.size();++i)
     {
         std::size_t idx = unknowns_original_indx[i];
-        classify_results.label[idx] = std::to_string(labels[i]);
+        final_result.label[idx] = std::to_string(labels[i]);
     }
+    std::cout << "result (in cluster label):" << std::endl;
     for(std::size_t i = 0;i < original_size;++i)
     {
-        std::cout << classify_results.label[i] << std::endl;
+        std::cout << final_result.label[i] << std::endl;
     }
 
+    // find corresponding label for each cluster
+    dataset test_data;
+    reader.read(argv[2], test_data);
+    std::vector<std::unordered_map<std::string, int>> lable_cnt;
+    lable_cnt.resize(2);
+    for(std::size_t i = 0;i < unknowns_original_indx.size();++i)
+    {
+        std::size_t idx = unknowns_original_indx[i];
+        std::size_t label_idx = static_cast<std::size_t>(std::stoull(final_result.label[idx]));
+        ++lable_cnt[label_idx][test_data.label[idx]];
+    }
+    std::vector<int> mx_cnt(lable_cnt.size(),0);
+    std::vector<std::string> mx_label(lable_cnt.size());
+    for(std::size_t i = 0;i < mx_cnt.size();++i)
+    {
+        for(auto& mp : lable_cnt[i])
+        {
+            if(mp.second > mx_cnt[i])
+            {
+                mx_cnt[i] = mp.second;
+                mx_label[i] = mp.first;
+            }
+        }
+    }
+    for(std::size_t i = 0;i < unknowns_original_indx.size();++i)
+    {
+        std::size_t idx = unknowns_original_indx[i];
+        std::size_t lable_idx = static_cast<std::size_t>(std::stoull(final_result.label[idx]));
+        final_result.label[idx] = mx_label[lable_idx];
+    }
+
+    // output result 
+    std::cout << "result (in class label):" << std::endl;
+    std::cout << "==============================" << std::endl;
+    for(std::size_t i = 0;i < mx_label.size();++i)
+    {
+        std::cout << "cluster " << i << ": ";
+        std::cout << mx_label[i] << std::endl;
+    }
+    std::cout << "==============================" << std::endl;
+    for(std::size_t i = 0;i < original_size;++i)
+    {
+        std::cout << final_result.label[i] << std::endl;
+    }
+
+    // calculate acc
+    int acc_cnt = 0;
+    for(std::size_t i = 0;i < test_data.size();++i)
+    {
+        if(final_result.label[i] == test_data.label[i])
+        {
+            ++acc_cnt;
+        }
+    }
+    std::cout << "Overall Accuracy: " << (double)acc_cnt/test_data.size() << std::endl;
     return 0;
 }
